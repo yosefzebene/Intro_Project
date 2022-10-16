f30f23ddcc37ea51c27f4d7baaabf0ca2cef7324
yosef-ZzZ <yosef.zebene1@gmail.com>
2022-10-16 01:51:28 -0500

    Updated the seed file to import data from the 311 API

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Request.destroy_all
+Service.destroy_all
+Category.destroy_all
+Neighbourhood.destroy_all
+Ward.destroy_all
+
+def fetch_data(url)
+    JSON.parse(open(url).read)
+end
+
+requests = fetch_data('https://data.winnipeg.ca/resource/4her-3th5.json')
+
+requests.each do |request|
+    ward_name = request['ward']
+    ward = Ward.find_or_create_by(name: ward_name)
+
+    neighbourhood = ward.neighbourhoods.find_or_create_by(
+        name: request['neighbourhood']
+    )
+
+    service_category_name = request['service_area']
+    category = Category.find_or_create_by(name:service_category_name)
+
+    service = category.services.find_or_create_by(
+        service_request: request['service_request']
+    )
+
+    request = service.requests.build(
+        neighbourhood_id: neighbourhood.id,
+        latitude: request['location_1']['latitude'],
+        longitude: request['location_1']['longitude'],
+        date: request['sr_date']
+    )
+    request.save
+end
